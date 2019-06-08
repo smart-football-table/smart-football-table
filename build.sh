616ecb6fa723f8cc6911446eb7824188ff171b8f
Peter Fichtner (pfichtner) <github.07.fichtner@spamgourmet.com>
2019-06-09 01:27:13 +0200

    detection added

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
 find . -maxdepth 1 -type d \( ! -name . \) -exec bash -c "cd '{}' && ( [ -e build.sh ] && ./build.sh )" \;
-# find . -maxdepth 1 -type d \( ! -name . \) -exec bash -c "cd '{}' && [ -r Dockerfile ] && docker build ." \;
 
